ddca30b708a2efbe8bd4c8373116e07d2b466bb9
DESKTOP-8PFT5MS\Hao <bluesky02031998@gmail.com>
2025-04-26 23:43:46 +0800

    add [ 串接API 城堡列表getCastleList ]

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -4,10 +4,10 @@
 
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       <div
-        v-for="(item, index) in 8"
+        v-for="(item, index) in list"
         :key="index"
         class="relative bg-gray-800 rounded-xl shadow-md p-6 hover:shadow-xl transition duration-300 bg-cover md:h-[200px] xl:h-[350px]"
-        :style="{ backgroundImage: `url(/card${index + 1}.jpg)` }"
+        :style="{ backgroundImage: `url(${item.image_url})` }"
       >
         <div class="absolute bottom-2 right-2 xl:bottom-4 xl:right-4">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-star-icon lucide-star"><path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/></svg>
@@ -17,8 +17,27 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { getCastleList } from '@/api/explore'
+import { Castle } from '@/types/castle'
+
+const list = ref<Castle[]>([])
+const total = ref<number>(0)
+
+onMounted(() => {
+  getList()
+})
+
+async function getList() {
+  const { data } = await getCastleList({
+    page: 1,
+    pageSize: 12,
+  })
+
+  list.value = data.castles
+  total.value = data.total
+}
+
 
 </script>
-<style>
-</style>
